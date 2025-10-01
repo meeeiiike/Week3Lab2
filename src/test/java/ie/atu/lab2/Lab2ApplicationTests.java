228218317ef3ef6601f4228d61bbc35772df88e3
G00407095@atu.ie <G00407095@atu.ie>
2025-10-01 14:21:57 +0100

    init commit for week3 lab2, includes all dependencies

---
+++ src/test/java/ie/atu/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
